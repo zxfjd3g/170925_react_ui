ebc39e6ee1de5607997bcc8fc20d87160b44ba36
zxfjd3g <258147149@qq.com>
2018-01-23 15:46:59 +0800

    init app

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {Button, Toast} from 'antd-mobile'
+
+export default class App extends Component {
+
+  handleClick = () => {
+    Toast.info('提交成功')
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type='primary' onClick={this.handleClick}>提交</Button>
+      </div>
+    )
+  }
+}
